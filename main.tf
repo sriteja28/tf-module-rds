e1c6437cb5e0f7874688e3bcbc64b685425c761e
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-10-03 19:52:11 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -31,16 +31,17 @@ resource "aws_db_subnet_group" "main" {
 
 
 resource "aws_rds_cluster" "main" {
-  cluster_identifier   = "${var.component}-${var.env}"
-  engine               = var.engine
-  engine_version       = var.engine_version
-  database_name        = var.db_name
-  master_username      = data.aws_ssm_parameter.username.value
-  master_password      = data.aws_ssm_parameter.password.value
-  db_subnet_group_name = aws_db_subnet_group.main.name
-  storage_encrypted    = true
-  kms_key_id           = var.kms_key_arn
+  cluster_identifier     = "${var.component}-${var.env}"
+  engine                 = var.engine
+  engine_version         = var.engine_version
+  database_name          = var.db_name
+  master_username        = data.aws_ssm_parameter.username.value
+  master_password        = data.aws_ssm_parameter.password.value
+  db_subnet_group_name   = aws_db_subnet_group.main.name
+  storage_encrypted      = true
+  kms_key_id             = var.kms_key_arn
   vpc_security_group_ids = [aws_security_group.sg.id]
+  skip_final_snapshot    = true
 }
 
 resource "aws_rds_cluster_instance" "cluster_instances" {
